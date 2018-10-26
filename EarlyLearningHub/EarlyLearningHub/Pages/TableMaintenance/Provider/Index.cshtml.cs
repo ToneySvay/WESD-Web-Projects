cc5140eecd4d6f57fd93171a06c3d25ecc5af525
Toney Svay <toney.svay@wesd.org>
2018-10-26 15:04:52 -0700

    Added sorting for Provider

--- EarlyLearningHub/EarlyLearningHub/Pages/TableMaintenance/Provider/Index.cshtml.cs
+++ EarlyLearningHub/EarlyLearningHub/Pages/TableMaintenance/Provider/Index.cshtml.cs
@@ -20,10 +20,50 @@ namespace EarlyLearningHub.Pages.TableMaintenance.Provider
 
         public IList<Models.Provider> Provider { get;set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string sortOrder)
         {
-            Provider = await _context.Provider
-                .Include(p => p.PrvdPt).ToListAsync();
+            NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            TypeSort = sortOrder == "Type" ? "type_desc" : "Type";
+
+            IQueryable<Models.Provider> providerIQ = from s in _context.Provider.Include(p => p.PrvdPt)
+                                                     select s;
+
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    providerIQ = providerIQ.OrderByDescending(s => s.PrvdName);
+                    break;
+                case "Type":
+                    providerIQ = providerIQ.OrderBy(s => s.PrvdPt);
+                    break;
+                case "type_desc":
+                    providerIQ = providerIQ.OrderByDescending(s => s.PrvdPt);
+                    break;
+                default:
+                    providerIQ = providerIQ.OrderBy(s => s.PrvdName);
+                    break;
+            }
+
+
+
+
+
+
+            //Provider = await _context.Provider.Include(p => p.PrvdPt).ToListAsync();
+            //Provider = await providerIQ.AsNoTracking().ToListAsync();
+            Provider = await providerIQ.AsNoTracking().ToListAsync();
+
+            //Provider = await PaginatedList<Models.Provider>.CreateAsync(providerIQ.AsNoTracking(), pageIndex ?? 1, pageSize);
+
+
         }
+
+        public string NameSort { get; set; }
+        public string TypeSort { get; set; }
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
     }
+
+
 }
