b4fe644c0f0c707d61108fb8bb822991d669f614
Toney Svay <toney.svay@wesd.org>
2018-10-26 14:01:08 -0700

    Added Provider Info

---
+++ EarlyLearningHub/EarlyLearningHub/Pages/TableMaintenance/Provider/Create.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using EarlyLearningHub.Models;
+
+namespace EarlyLearningHub.Pages.TableMaintenance.Provider
+{
+    public class CreateModel : PageModel
+    {
+        private readonly EarlyLearningHub.Models.EarlyLearningHubContext _context;
+
+        public CreateModel(EarlyLearningHub.Models.EarlyLearningHubContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+        ViewData["PrvdPtId"] = new SelectList(_context.ProviderType, "PtId", "PtName");
+            return Page();
+        }
+
+        [BindProperty]
+        public Models.Provider Provider { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Provider.Add(Provider);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ EarlyLearningHub/EarlyLearningHub/Pages/TableMaintenance/Provider/Edit.cshtml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EarlyLearningHub.Models;
+
+namespace EarlyLearningHub.Pages.TableMaintenance.Provider
+{
+    public class EditModel : PageModel
+    {
+        private readonly EarlyLearningHub.Models.EarlyLearningHubContext _context;
+
+        public EditModel(EarlyLearningHub.Models.EarlyLearningHubContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Models.Provider Provider { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Provider = await _context.Provider
+                .Include(p => p.PrvdPt).FirstOrDefaultAsync(m => m.PrvdId == id);
+
+            if (Provider == null)
+            {
+                return NotFound();
+            }
+           ViewData["PrvdPtId"] = new SelectList(_context.ProviderType, "PtId", "PtName");
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Provider).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProviderExists(Provider.PrvdId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool ProviderExists(int id)
+        {
+            return _context.Provider.Any(e => e.PrvdId == id);
+        }
+    }
+}
