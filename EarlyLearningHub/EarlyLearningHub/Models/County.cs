a44612544501719e995a94543b896883ef455558
Toney Svay <toney.svay@wesd.org>
2018-10-26 11:44:42 -0700

    Added County Attributes

--- EarlyLearningHub/EarlyLearningHub/Models/County.cs
+++ EarlyLearningHub/EarlyLearningHub/Models/County.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EarlyLearningHub.Models
 {
     public partial class County
     {
         public int CoId { get; set; }
+        [Required]
+        [StringLength(100, ErrorMessage = "County name cannot be longer than 100 characters.")]
+        [Column("CO_Name")]
+        [Display(Name = "County")]
         public string CoName { get; set; }
     }
 }
